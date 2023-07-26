187165d0fd72af190bc8135939589295e8a20260
Amara Ceresi <aceresi95@gmail.com>
2023-07-26 18:10:22 -0500

    Refactor server setup to include auth middleware and error handling

--- server/server.js
+++ server/server.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
 const { typeDefs, resolvers } = require('./schemas');
-const connectDB = require('./config/connection');
+const authMiddleware = require('./utils/auth');
+const db = require('./config/connection');
 
-require('dotenv').config();
-
-connectDB();
-
-const app = express();
 const PORT = process.env.PORT || 3002;
+const app = express();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-server.start().then(() => {
+async function startApolloServer() {
+  await server.start();
   server.applyMiddleware({ app });
 
-  app.listen(PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
-});
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+}
+
+// Global error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+startApolloServer();
