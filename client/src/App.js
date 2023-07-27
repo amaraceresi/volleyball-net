7cf59c952f46eee12126cf87f800a7ff3c0c0008
Amara Ceresi <aceresi95@gmail.com>
2023-07-27 15:12:23 -0500

    Define routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard/Dashboard";
+import HomePage from "./pages/HomePage/HomePage";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import TournamentDetails from "./pages/TournamentDetails/TournamentDetails";
+import TournamentList from "./pages/TournamentList/TournamentList";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3002/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tournaments" element={<TournamentList />} />
+          <Route path="/tournament/:id" element={<TournamentDetails />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,19 @@
+// src/pages/Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Volleyball Website!</h1>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+      <Link to="/signup">
+        <button>Sign Up</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
