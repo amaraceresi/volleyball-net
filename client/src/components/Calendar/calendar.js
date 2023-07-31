11318bd92008203953737287c5307cd57b345f72
Amara Ceresi <aceresi95@gmail.com>
2023-07-31 17:01:57 -0500

    Add ages for register

--- client/src/components/Calendar/calendar.js
+++ client/src/components/Calendar/calendar.js
@@ -1,55 +1,78 @@
 import { useSelector } from 'react-redux';
 import { getUser } from '../../redux/slices/userSlice';
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar, { EventContentArg } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Select from 'react-dropdown-select';
 
 const events = [
   {
     id: 1,
     title: "Test Event 1",
-    start: "2023-07-14",
-    end: "2023-07-20",
+    start: "2023-07-15",
+    end: "2023-07-15",
   },
   {
     id: 2,
     title: "Test Event 2",
-    start: "2023-07-16",
-    end: "2023-07-21",
+    start: "2023-07-23",
+    end: "2023-07-23",
   },
+];
+
+const options = [
+  { value: '16', label: '16U Register' },
+  { value: '14', label: '14U Register' },
+  { value: '12', label: '12U Register' },
 ];
 
 export default function Calendar() {
   const { userData } = useSelector(getUser());
   const { role } = userData;
 
-  const handleEventClick = (obj) => {
-    const { event } = obj;
+  const handleEventClick = ({ event }) => {
     console.log("Event Selected");
     console.log(event);
   };
 
+  const EventContent = ({ event }: EventContentArg) => (
+    <div>
+      <b>{event.title}</b>
+      <Select
+        options={options}
+        onChange={(selected) => {
+          window.location.href = `/register/${event.id}/${selected[0].value}`;
+        }}
+      />
+    </div>
+  );
+
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      selectable={true}
-      editable={true}
-      droppable={true}
-      select={(info) => {
-        console.log("date selected");
-        if (role === 'Admin') {
-          // Do something if the user is an admin
-        }
-        else {
-          // Display age groups and have them selectable
-          // Register team from age group
-        }
-        console.log(info);
-      }}
-      events={events}
-      eventClick={handleEventClick}
-      // aspectRatio={2.5}
-    />
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        selectable={true}
+        editable={true}
+        droppable={true}
+        select={(info) => {
+          console.log("date selected");
+          if (role === 'Admin') {
+            // Do something if the user is an admin
+          }
+          else {
+            // Display age groups and have them selectable
+            // Register team from age group
+          }
+          console.log(info);
+        }}
+        events={events}
+        eventClick={handleEventClick}
+        eventContent={EventContent}
+        // aspectRatio={2.5}
+      />
+    </div>
   );
 }
