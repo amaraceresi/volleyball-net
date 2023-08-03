20fde1cb826adc43ba4c606b349c3d9145f83eb9
Amara Ceresi <aceresi95@gmail.com>
2023-08-02 19:49:28 -0500

    stripe

--- client/src/pages/Payment/Payment.jsx
+++ client/src/pages/Payment/Payment.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 function Payment() {
 const navigate = useNavigate();
 
-const handlePayment = () => {
-// Handle payment process here. After successful payment, navigate back to registration page
-console.log("Processing payment...");
-navigate('/register');
+const handlePayment = async () => {
+  const stripe = await stripePromise;
+
+  const { error } = await stripe.redirectToCheckout({
+    lineItems: [{ price: 'price_1JHABC2eZvKYlo2CZwxd92Kl', quantity: 1 }],
+    mode: 'payment',
+    successUrl: `${window.location.origin}/register?success=true`,
+    cancelUrl: `${window.location.origin}/register?canceled=true`,
+  });
+
+  if (error) {
+    console.warn('Error:', error);
+  }
 };
 
 return (
