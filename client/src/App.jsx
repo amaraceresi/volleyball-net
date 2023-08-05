6a43d26bb33bbb39b0f70c029320fae00810ab0f
Amara Ceresi <aceresi95@gmail.com>
2023-08-05 11:36:08 -0500

    added map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,16 @@ import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from "./redux/store";
-import Auth from './components/Auth'
+import Auth from './components/Auth';
 import Dashboard from "./pages/Dashboard/Dashboard";
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Tournaments from "./pages/Tournaments/Tournaments";
-import Payment from "./pages/Payment/Payment"
-import Register from "./pages/Register/Register"
+import Payment from "./pages/Payment/Payment";
+import Register from "./pages/Register/Register";
 import Navbar from "./components/Navbar/Navbar";
+import Contact from "./pages/Contact/Contact";
 import { HelmetProvider } from 'react-helmet-async'
 
 const httpLink = createHttpLink({
@@ -44,6 +45,7 @@ function App() {
               <Navbar />
               <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/contact" element={<Contact />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/dashboard" element={<Dashboard />} />
--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import GoogleMapReact from 'google-map-react';
 import Page from "../../components/Page";
 import './HomePage.css';
 
@@ -11,6 +12,8 @@ const headContent = (
   </>
 );
 
+const MapComponent = ({ text }) => <div>{text}</div>;
+
 const HomePage = () => {
   const navigate = useNavigate();
   const isAuthenticated = useSelector(state => state.auth?.isAuthenticated);
@@ -20,6 +23,12 @@ const HomePage = () => {
       navigate('/dashboard');
     }
   }, [isAuthenticated, navigate]);
+
+  const center = {
+    lat: 37.0565,
+    lng: -86.2089
+  };
+  const zoom = 11;
 
   return (
     <Page isProtected={false} headContent={headContent}>
@@ -32,6 +41,19 @@ const HomePage = () => {
           <Link to="/signup" className="linkStyle">
             <button className="buttonStyle">Sign Up</button>
           </Link>
+        </div>
+        <div style={{ height: '50vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'YOUR_GOOGLE_MAPS_API_KEY' }} 
+            defaultCenter={center}
+            defaultZoom={zoom}
+          >
+            <MapComponent
+              lat={37.0565}
+              lng={-86.2089}
+              text={'795 Noah Bledsoe Rd, Smiths Grove, KY 42171'}
+            />
+          </GoogleMapReact>
         </div>
       </div>
     </Page>
